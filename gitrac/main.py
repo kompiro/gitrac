35b719a0c61430c13be2fce59645f7c5ff73a390
Hiroki Kondo <kompiro@gmail.com>
2009-11-25 23:28:53 +0900

    created package

--- gitrac/main.py
+++ gitrac/main.py
@@ -1,61 +1,56 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import os,os.path
 import sys
 import sqlite3
 import xmlrpclib
+import ConfigParser
 
-USERNAME="kompiro"
-PASSWORD="trac"
-REPOSITORY_URL="localhost:8080/test/login/xmlrpc"
-SERVER_URL = "http://" + USERNAME + ":" + PASSWORD +"@" + REPOSITORY_URL
-home = os.environ['HOME'];
+home = os.environ['PWD'];
 local_trac_dir = home+'/.trac';
-
-def main():
-  init_ticket_db();
-  parse_opt();
 
 def parse_opt():
   if len(sys.argv) == 1:
-    usage();
+    help();
     return;
   opt = sys.argv[1]
-  if opt == "clone":
-      url = sys.argv[2]
-      create_ticket_repository(url);
+  if opt == "init":
+    url = sys.argv[2]
+    create_ticket_repository(url);
+    return;
+
+  init_ticket_db();
   if opt == "list":
     ticket_list();
+    return;
   if opt == "add":
     summary = sys.argv[2]
     ticket_add(summary);
+    return;
   if opt == "push":
     ticket_push();
+    return;
   if opt == "help":
-      usage();
-
-def usage():
-  usage_message = """
-gitrac [command] 
-
-  command:
-    %s""" % ticket_list.__doc__;
-  print usage_message;
+    help();
+    return;
 
 def create_ticket_repository(url):
-    import ConfigParser
-    config = ConfigParser.RawConfigParser()
-    config.read('.trac/settings.conf');
-
-
-def init_ticket_db():
+  """ init [url] - initialized repository setting """
+  config = ConfigParser.RawConfigParser();
   if os.path.exists(local_trac_dir) == False:
     os.mkdir(local_trac_dir);
+  path = __get_config_path__()
+  config.add_section("REPOSITORY")
+  config.set("REPOSITORY","URL",url);
+  config.write(open(path,"w"));
+  init_ticket_db();
+
+def init_ticket_db():
   global db;
   db = sqlite3.connect(local_trac_dir + '/ticket.db',isolation_level=None);
-  if not_exists_local_ticket():
-    create_local_ticket()
+  create_local_ticket()
+  #if not_exists_local_ticket():
   return db;
 
 def not_exists_local_ticket():
@@ -67,7 +62,7 @@ def not_exists_local_ticket():
     return count[0] == 0;
 
 def create_local_ticket():
-  sql = u"""create table local_ticket (
+  sql = u"""CREATE TABLE IF NOT EXISTS local_ticket (
     id integer primary key autoincrement,
     ticket_id integer,
     summary TEXT,
@@ -76,7 +71,7 @@ def create_local_ticket():
   db.execute(sql);
 
 def ticket_list():
-  """   list - show local ticket list """
+  """ list - show local ticket list """
   for id,ticket_id,summary,close in db.execute('select * from local_ticket'):
     resistered = ticket_id != None
     if resistered:
@@ -86,22 +81,48 @@ def ticket_list():
     print id,show,summary,close == 1
 
 def ticket_add(summary):
-  """
-  create ticket to local ticket
-  """
+  """ add [summary] - create ticket to local ticket """
   sql = u"""
   insert into local_ticket values(null,?,?,?)
   """;
   db.execute(sql,(None,summary.decode('utf-8'),False));  
 
 def ticket_push():
-  server = xmlrpclib.ServerProxy(SERVER_URL)
+  """ push - push tickets to Trac repository """
+  config = ConfigParser.RawConfigParser();
+  path = __get_config_path__()
+  config.read(path);
+  url = config.get("REPOSITORY","URL")
+  server = xmlrpclib.ServerProxy(url);
   for id,ticket_id,summary,close in db.execute('select * from local_ticket'):
     resistered = ticket_id != None
     if resistered == False:
       registered_id = server.ticket.create(summary,"",{'type':'defect'})
       print "created", registered_id,summary;
       db.execute("update local_ticket set ticket_id=? where id = ?",(registered_id,id))
+
+def help():
+  """ help - showing message """
+  usage_message = """Usage: gitrac [command] 
+  command:
+    %s
+    %s
+    %s
+    %s
+    %s""" % (
+    create_ticket_repository.__doc__,
+    ticket_add.__doc__,
+    ticket_list.__doc__,
+    ticket_push.__doc__,
+    help.__doc__
+    );
+  print usage_message;
+
+def __get_config_path__():
+  return local_trac_dir + '/settings.conf';
+
+def main():
+  parse_opt();
 
 if __name__=='__main__':
   main();
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 Hiroki Kondo (aka.kompiro)
+# All rights reserved.
+
+from setuptools import setup
+
+setup(
+    name = 'gitrac',
+    version = '0.0.1',
+    description = 'distributed Trac Ticket repository like git',
+    author = 'Hiroki Kondo',
+    author_email = 'kompiro@gmail.com',
+    license = 'EPL',
+    url = 'http://github.com/kompiro/gitrac',
+    download_url = 'http://github.com/kompiro/gitrac/downloads',
+    packages = ['gitrac'],
+    entry_points = """
+        [console_scripts]
+        gitrac = gitrac.main:main
+    """,
+)
